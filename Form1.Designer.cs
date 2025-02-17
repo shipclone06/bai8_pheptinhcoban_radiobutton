38095b254e38cf86c18e06d9dfc6ef9d04afc2c7
Administrator <Administrator@LAB1-MAY04>
2025-02-17 11:31:44 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace bai8_pheptinhcoban_radiobutton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.RDtru = new System.Windows.Forms.RadioButton();
+            this.RDcong = new System.Windows.Forms.RadioButton();
+            this.RDchia = new System.Windows.Forms.RadioButton();
+            this.RDnhan = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RDnhan);
+            this.groupBox1.Controls.Add(this.RDcong);
+            this.groupBox1.Controls.Add(this.RDchia);
+            this.groupBox1.Controls.Add(this.RDtru);
+            this.groupBox1.Location = new System.Drawing.Point(545, 103);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 188);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "chon phep tinh";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // RDtru
+            // 
+            this.RDtru.AutoSize = true;
+            this.RDtru.Location = new System.Drawing.Point(17, 49);
+            this.RDtru.Name = "RDtru";
+            this.RDtru.Size = new System.Drawing.Size(37, 17);
+            this.RDtru.TabIndex = 0;
+            this.RDtru.TabStop = true;
+            this.RDtru.Text = "tru";
+            this.RDtru.UseVisualStyleBackColor = true;
+            // 
+            // RDcong
+            // 
+            this.RDcong.AutoSize = true;
+            this.RDcong.Location = new System.Drawing.Point(17, 26);
+            this.RDcong.Name = "RDcong";
+            this.RDcong.Size = new System.Drawing.Size(49, 17);
+            this.RDcong.TabIndex = 0;
+            this.RDcong.TabStop = true;
+            this.RDcong.Text = "cong";
+            this.RDcong.UseVisualStyleBackColor = true;
+            this.RDcong.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // RDchia
+            // 
+            this.RDchia.AutoSize = true;
+            this.RDchia.Location = new System.Drawing.Point(17, 93);
+            this.RDchia.Name = "RDchia";
+            this.RDchia.Size = new System.Drawing.Size(45, 17);
+            this.RDchia.TabIndex = 0;
+            this.RDchia.TabStop = true;
+            this.RDchia.Text = "chia";
+            this.RDchia.UseVisualStyleBackColor = true;
+            this.RDchia.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // RDnhan
+            // 
+            this.RDnhan.AutoSize = true;
+            this.RDnhan.Location = new System.Drawing.Point(17, 70);
+            this.RDnhan.Name = "RDnhan";
+            this.RDnhan.Size = new System.Drawing.Size(49, 17);
+            this.RDnhan.TabIndex = 0;
+            this.RDnhan.TabStop = true;
+            this.RDnhan.Text = "nhan";
+            this.RDnhan.UseVisualStyleBackColor = true;
+            this.RDnhan.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 280);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "label1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(81, 132);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton RDnhan;
+        private System.Windows.Forms.RadioButton RDcong;
+        private System.Windows.Forms.RadioButton RDchia;
+        private System.Windows.Forms.RadioButton RDtru;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai8_pheptinhcoban_radiobutton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double so1 = double.Parse(textBox2.Text);
+            double so2 = double.Parse(textBox2.Text);
+            if (RDcong.Checked)
+                {
+                textBox2 = new TextBox();
+        }
+    }
+}
